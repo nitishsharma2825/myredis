3a75c4e634bf91359bb4e5a0e9b98be1f5166fd6
nitishsharma2825 <sharnit2825@gmail.com>
2025-01-31 20:59:44 +0530

    key-value store (defining the protocol)

--- client.cpp
+++ client.cpp
@@ -23,7 +23,7 @@ static void die(const char* msg) {
 
 const size_t k_max_msg = 32 << 20; // likely larger than the kernel buffer
 
-static int32_t read_full(int fd, uint8_t* buf, size_t n) {
+static int32_t read_full(int fd, char* buf, size_t n) {
     while (n > 0) {
         size_t rv = read(fd, buf, n);
         if (rv <= 0 ) {
@@ -37,7 +37,7 @@ static int32_t read_full(int fd, uint8_t* buf, size_t n) {
     return 0;
 }
 
-static int32_t write_all(int fd, const uint8_t* buf, size_t n) {
+static int32_t write_all(int fd, const char* buf, size_t n) {
     while (n > 0) {
         size_t rv = write(fd, buf, n);
         if (rv <= 0) {
@@ -56,55 +56,70 @@ static void buf_append(std::vector<uint8_t>& buf, const uint8_t* data, size_t le
     buf.insert(buf.end(), data, data + len);
 }
 
-// the `query` function was simply splitted inti 'send_req' and 'read_res'
-static int32_t send_req(int fd, const uint8_t* text, size_t len) {
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
+    }
     if (len > k_max_msg) {
+        msg("message too long");
         return -1;
     }
 
-    std::vector<uint8_t> wbuf;
-    buf_append(wbuf, (const uint8_t*)&len, 4);
-    buf_append(wbuf, text, len);
-    return write_all(fd, wbuf.data(), wbuf.size());
+    char wbuf[4 + k_max_msg];
+    memcpy(&wbuf[0], &len, 4);
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd) {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
+    return write_all(fd, wbuf, 4 + len);
 }
 
 static int32_t read_res(int fd) {
     // 4 bytes header
-    std::vector<uint8_t> rbuf;
-    rbuf.resize(4);
+    char rbuf[4 + k_max_msg + 1];
     errno = 0;
-    int32_t err = read_full(fd, &rbuf[0], 4);
+    int32_t err = read_full(fd, rbuf, 4);
     if (err) {
         if (errno == 0) {
             msg("EOF");
         } else {
-            msg("read error");
+            msg("read() error");
         }
         return err;
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf.data(), 4);
+    memcpy(&len, rbuf, 4);
     if (len > k_max_msg) {
         msg("message too long");
         return -1;
     }
 
     // reply body
-    rbuf.resize(4 + len);
     err = read_full(fd, &rbuf[4], len);
     if (err) {
-        msg("read error");
+        msg("read() error");
         return err;
     }
 
-    // do something
-    printf("server says: len:%u data:%.*s\n",
-            len, len < 100 ? len : 100, &rbuf[4]);
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
 
-int main() {
+int main(int argc, char** argv) {
     // create a socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -120,28 +135,20 @@ int main() {
         die("connect()");
     }
 
-    // multiple pipelined requests
-    std::vector<std::string> query_list = {
-        "hello1", "hello2", "hello3",
-        // a large message requires multiple event loop iterations
-        std::string(4096, 'z'),
-        "hello5",
-    };
-
-    for (const std::string &s: query_list) {
-        int32_t err = send_req(fd, (uint8_t*)s.data(), s.size());
-        if (err) {
-            goto L_DONE;
-        }
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cmd.push_back(argv[i]);
     }
 
-    for (size_t i = 0; i < query_list.size(); ++i) {
-        int32_t err = read_res(fd);
-        if (err) {
-            goto L_DONE;
-        }
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
     }
-
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
+    }
+    
 L_DONE:
     close(fd);
     return 0;
--- server.cpp
+++ server.cpp
@@ -16,6 +16,8 @@
 
 // c++
 #include <vector>
+#include <string>
+#include <map>
 
 static void msg(const char* msg) {
     fprintf(stderr, "%s\n", msg);
@@ -96,6 +98,97 @@ static Conn* handle_accept(int fd) {
     return conn;
 }
 
+static bool read_u32(const uint8_t* &cur, const uint8_t* end, uint32_t& out) {
+    if (cur + 4 > end) {
+        return false;
+    }
+    memcpy(&out, cur, 4);
+    cur+=4;
+    return true;
+}
+
+static bool read_str(const uint8_t* &cur, const uint8_t* end, size_t n, std::string &out) {
+    if (cur + n > end) {
+        return false;
+    }
+    out.assign(cur, cur + n);
+    cur += n;
+    return true;
+}
+
+// +------+-----+------+-----+------+-----+-----+------+
+// | nstr | len | str1 | len | str2 | ... | len | strn |
+// +------+-----+------+-----+------+-----+-----+------+
+
+static int32_t parse_req(const uint8_t* data, size_t size, std::vector<std::string> &out) {
+    const uint8_t *end = data + size;
+    uint32_t nstr = 0;
+    if (!read_u32(data, end, nstr)){
+        return -1;
+    }
+    if (nstr > k_max_msg){
+        return -1; // safety limit
+    }
+
+    while (out.size() < nstr) {
+        uint32_t len = 0;
+        if (read_u32(data, end, len)) {
+            return -1;
+        }
+        out.push_back(std::string());
+        if (!read_str(data, end, len, out.back())) {
+            return -1;
+        }        
+    }
+    if (data != end) {
+        return -1; // trailing garbage
+    }
+    return 0;
+}
+
+// Response::status
+enum {
+    RES_OK = 0,
+    RES_ERR = 1, // error
+    RES_NX = 2,  // not found
+};
+
+// +--------+---------+
+// | status | data... |
+// +--------+---------+
+struct Response {
+    uint32_t status = 0;
+    std::vector<uint8_t> data;
+};
+
+// placeholder: implemented later
+static std::map<std::string, std::string> g_data;
+
+static void do_request(std::vector<std::string> &cmd, Response &out) {
+    if (cmd.size() == 2 && cmd[0] == "get") {
+        auto it = g_data.find(cmd[1]);
+        if (it == g_data.end()) {
+            out.status = RES_NX; // not found
+            return;
+        }
+        const std::string &val = it->second;
+        out.data.assign(val.begin(), val.end());
+    } else if (cmd.size() == 3 && cmd[0] == "set") {
+        g_data[cmd[1]].swap(cmd[2]);
+    } else if (cmd.size() == 2 && cmd[0] == "del") {
+        g_data.erase(cmd[1]);
+    } else {
+        out.status = RES_ERR; // unrecognized command
+    }
+}
+
+static void make_response(const Response &resp, std::vector<uint8_t> &out) {
+    uint32_t resp_len = 4 + (uint32_t)resp.data.size();
+    buf_append(out, (const uint8_t*)&resp_len, 4);
+    buf_append(out, (const uint8_t*)&resp.status, 4);
+    buf_append(out, resp.data.data(), resp.data.size());
+}
+
 // process 1 request if there is enough data
 static bool try_one_request(Conn* conn) {
     // try to parse the protocol: message header
@@ -118,17 +211,20 @@ static bool try_one_request(Conn* conn) {
     const uint8_t* request = &conn->incoming[4];
 
     // got one request, do some application logic
-    printf("client says: len:%d data:%.*s\n",
-            len, len < 100 ? len : 100, request);
-    
-    // generate a response (echo)
-    buf_append(conn->outgoing, (const uint8_t*)&len, 4);
-    buf_append(conn->outgoing, request, len);
+    std::vector<std::string> cmd;
+    if (parse_req(request, len, cmd) < 0) {
+        msg("bad request");
+        conn->want_close = true;
+        return false; // want close
+    }
+
+    Response resp;
+    do_request(cmd, resp);
+    make_response(resp, conn->outgoing);
 
     // application logic done! remove the request message
-    // conn->incoming.clear(); // wrong!
     buf_consume(conn->incoming, 4 + len);
-    // Q. Why not just empty the buffer? See the explanation of "pipelining"
+    // Q. Why not just empty the buffer? see the explanation of "pipelining"
     return true; // success
 }
 
